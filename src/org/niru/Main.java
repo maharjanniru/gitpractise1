e556e44afc956c8d8a5a3ed04bf9dafa4b24b728
maharjanniru <62689656+maharjanniru@users.noreply.github.com>
2020-04-13 16:54:19 +0545

    Update Main.java

--- src/org/niru/Main.java
+++ src/org/niru/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Happy new year 2077");
         System.out.println("from feature branch");
-        Sytem.out.println("new line added from github");
+        System.out.println("new line added from github");
 
     }
 }
