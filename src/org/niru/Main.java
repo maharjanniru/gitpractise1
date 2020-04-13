93737a3d7ceae31d1225ce10a36f8c743e3e37d4
maharjanniru <nmaharjan@deerhold.org>
2020-04-13 16:51:49 +0545

    "inital commit"

--- src/org/niru/Main.java
+++ src/org/niru/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Happy new year 2077");
+        System.out.println("from feature branch");
 
     }
 }
